6078d76eed898224a419c18066ffa3564fb38fac
Phan Hoang <dev.phansihoang@gmail.com>
2025-07-13 18:41:55 +0700

    Refactor Prisma setup and update database connection; removed deprecated postinstall script, replaced prisma.ts with db.ts for improved client management, and adjusted session handling in the Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,19 @@ import { login, logout } from "@/lib/actions/auth";
 
 export default async function Home() {
   const session = await auth()
-  if (!session) return <div className="grid items-center justify-items-center h-screen font-sans">
-  <div className="">
-    <h1 className="text-8xl">You are not signed in</h1>
-    <form
-      action={login}
-    >
-        <Button type="submit" size="lg">Signin with GitHub</Button>
-    </form>
-  </div>
-</div>
+
+  if (!session) return (
+    <div className="grid items-center justify-items-center h-screen font-sans">
+      <div className="">
+        <h1 className="text-8xl">You are not signed in</h1>
+        <form
+          action={login}
+        >
+            <Button type="submit" size="lg">Signin with GitHub</Button>
+        </form>
+      </div>
+    </div>
+  )
 
   return (
     <div className="grid items-center justify-items-center h-screen font-sans">
