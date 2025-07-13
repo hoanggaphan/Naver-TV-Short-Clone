55a0b30e2b5f671532728cca84ab2528b68727fa
Phan Hoang <dev.phansihoang@gmail.com>
2025-07-13 17:25:12 +0700

    Update dependencies to Prisma 6.11.1 and configure database connection; added support for Neon adapter and adjusted build scripts for Prisma generation.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaClient } from '@prisma/client'
 
+// Setup
+const connectionString = `${process.env.DATABASE_URL}`
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+// Init prisma client
+const adapter = new PrismaNeon({ connectionString })
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
